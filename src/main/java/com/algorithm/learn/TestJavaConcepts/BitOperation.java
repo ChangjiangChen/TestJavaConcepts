3bc346f1e0d3ca2e782788f2d0e14119799cf5ae
BruceChan <changjiang.chen@changhong.com>
2017-09-04 13:53:54 +0800

    bitOperation and randomTest

---
+++ src/main/java/com/algorithm/learn/TestJavaConcepts/BitOperation.java
@@ -0,0 +1,14 @@
+package com.algorithm.learn.TestJavaConcepts;
+
+/**
+ * 位运算，java数的进制相关内容
+ * 
+ * @author changjiang.chen
+ *
+ */
+public class BitOperation {
+	public void testBitOperation() {
+		int i = (1 << 31) - 1;
+		System.out.println(i);
+	}
+}
---
+++ src/main/java/com/algorithm/learn/TestJavaConcepts/RandomTest.java
@@ -0,0 +1,14 @@
+package com.algorithm.learn.TestJavaConcepts;
+
+/**
+ * 未形成体系的内容，都放入该类中，做测试用；<br>
+ * 各个测试类进行结果展示的函数
+ * 
+ * @author changjiang.chen
+ *
+ */
+public class RandomTest {
+	public static void main(String[] args) {
+		new BitOperation().testBitOperation();
+	}
+}
