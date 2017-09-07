51a468ceddaa4e931bbdd04c05e026a492918548
BruceChan <changjiang.chen@changhong.com>
2017-09-07 22:03:21 +0800

    hashMap learning.

---
+++ src/main/java/com/algorithm/learn/TestJavaConcepts/concepts/HashMapTest.java
@@ -0,0 +1,10 @@
+package com.algorithm.learn.TestJavaConcepts.concepts;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+public class HashMapTest {
+	public void synchronizedMap() {
+		Collections.synchronizedMap(new HashMap<>());
+	}
+}
