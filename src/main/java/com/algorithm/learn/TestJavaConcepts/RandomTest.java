02fd5e0bcb3fda84840c47f7a8ed9cb7b9710015
BruceChan <changjiang.chen@changhong.com>
2017-09-07 08:40:02 +0800

    conflict solved.

--- src/main/java/com/algorithm/learn/TestJavaConcepts/RandomTest.java
+++ src/main/java/com/algorithm/learn/TestJavaConcepts/RandomTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.algorithm.learn.TestJavaConcepts;
 
 /**
@@ -16,22 +15,4 @@ public class RandomTest {
 		bo.testBitTransfer();
 	}
 }
-=======
-package com.algorithm.learn.TestJavaConcepts;
 
-/**
- * 未形成体系的内容，都放入该类中，做测试用；<br>
- * 各个测试类进行结果展示的函数
- * 
- * @author changjiang.chen
- *
- */
-public class RandomTest {
-	public static void main(String[] args) {
-		BitOperation bo = new BitOperation();
-		bo.testBitValue();
-		bo.testBitOperation();
-		bo.testBitTransfer();
-	}
-}
->>>>>>> 178dc578bf0d11735af4ba8a31000d41f1e33507
